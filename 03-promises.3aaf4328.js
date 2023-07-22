2b2d46f1d8a5a9584c6aa2cf3547d2c07f3f2c70
boblmbq <126655174+boblmbq@users.noreply.github.com>
2023-07-22 13:29:15 +0000

    Deploying to gh-pages from @ boblmbq/goit-js-hw-09@de0d440140c18077cec74fcd81886cb317f48ac5 🚀

---
+++ 03-promises.3aaf4328.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector(".form");function n(t,n){Math.random()}t.addEventListener("submit",(function(e){e.preventDefault();var r=new FormData(t),o=Object.fromEntries(r),a=(o.delay,o.step),u=o.amount;!function(t,e,r){for(var o=0;o<r;o++)n(),e,1}(0,a,u)}))}();
+//# sourceMappingURL=03-promises.3aaf4328.js.map
---
+++ 03-promises.beb139e4.js
@@ -0,0 +1,2 @@
+const t=document.querySelector(".form");function e(t,e){Math.random()}t.addEventListener("submit",(function(n){n.preventDefault();const o=new FormData(t),r=Object.fromEntries(o),{delay:a,step:c,amount:u}=r;!function(t,n,o){let r=t,a=1;for(let t=0;t<o;t++)e(),r+=n,a+=1}(a,c,u)}));
+//# sourceMappingURL=03-promises.beb139e4.js.map
